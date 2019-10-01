27e79d67d4a43c2c37f3b956443c168803e7a12c
Chloe Culver <c.culver831@gmail.com>
2019-09-30 17:11:48 -0700

    working on player spwan

--- HighNoonSimulator/Assets/Scripts/Respawn.cs
+++ HighNoonSimulator/Assets/Scripts/Respawn.cs
@@ -8,39 +8,43 @@ public class Respawn : MonoBehaviour
     public GameObject Spawn2;
     public GameObject Spawn3;
     public GameObject Spawn4;
+    public GameObject Player_n;
     public int spawnPoints = 4;
 
     public void respawn(GameObject player)
     {
         Debug.Log("Changing Spawn location");
         int spawnPoint = Random.Range(0, spawnPoints);
-        switch(spawnPoint)
-            {
-            case 0:
-                Debug.Log("Changing Spawn location 1");
-                Vector3 Spawn_ = new Vector3(Spawn1.transform.position.x, Spawn1.transform.position.y, Spawn1.transform.position.z);
-                Instantiate(player, Spawn_, Quaternion.identity);
-                player.GetComponent<PlayerDamage>().enemyHealth = 10;
-                break;
-            case 1:
-                Debug.Log("Changing Spawn location 2");
-                Vector3 Spawn_2 = new Vector3(Spawn2.transform.position.x, Spawn2.transform.position.y, Spawn2.transform.position.z);
-                Instantiate(player, Spawn_2, Quaternion.identity);
-                player.GetComponent<PlayerDamage>().enemyHealth = 10;
-                break;
-            case 2:
-                Debug.Log("Changing Spawn location 3");
-                Vector3 Spawn_3 = new Vector3(Spawn3.transform.position.x, Spawn3.transform.position.y, Spawn3.transform.position.z);
-                Instantiate(player, Spawn_3, Quaternion.identity);
-                player.GetComponent<PlayerDamage>().enemyHealth = 10;
-                break;
-            case 4:
-                Debug.Log("Changing Spawn location 4");
-                Vector3 Spawn_4 = new Vector3(Spawn4.transform.position.x, Spawn4.transform.position.y, Spawn4.transform.position.z);
-                Instantiate(player, Spawn_4, Quaternion.identity);
-                player.GetComponent<PlayerDamage>().enemyHealth = 10;
-                break;
+        Player_n = player;
+        player.transform.position = Spawn2.transform.position;
+        //switch (spawnPoint)
+        //    {
 
-        }   
+        //    case 0:
+        //        Debug.Log("Changing Spawn location 1");
+        //        player.transform.position = Spawn1.transform.position;
+        //        player.GetComponent<PlayerDamage>().enemyHealth = 10;
+
+        //        break;
+        //    case 1:
+        //        Debug.Log("Changing Spawn location 2");
+        //        player.transform.position = Spawn2.transform.position;
+        //        //Instantiate(player,( Spawn2.transform.position.x, Spawn2.transform.position.y, Spawn2.transform.position.z),);
+        //        //player.GetComponent<PlayerDamage>().enemyHealth = 10;
+        //        break;
+        //    case 2:
+        //        Debug.Log("Changing Spawn location 3");
+        //        player.transform.position = Spawn3.transform.position;
+        //        player.GetComponent<PlayerDamage>().enemyHealth = 10;
+        //        break;
+        //    case 4:
+        //        Debug.Log("changing spawn location 4");
+        //        player.transform.position = Spawn4.transform.position;
+        //        player.GetComponent<PlayerDamage>().enemyHealth = 10;
+        //        break;
+
+
+
+        //}   
     }
 }
