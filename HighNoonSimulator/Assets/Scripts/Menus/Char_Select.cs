825c9322aedb4780e8476d17bb08c119354c7931
Chloe Culver <c.culver831@gmail.com>
2019-10-15 13:09:20 -0700

    Working on lobby join

---
+++ HighNoonSimulator/Assets/Scripts/Menus/Char_Select.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Char_Select : MonoBehaviour
+{
+    public GameObject PlayerSelect;
+    public GameObject LobbyUI;
+
+    public void Play()
+    {
+        PlayerSelect.SetActive(false);
+        LobbyUI.SetActive(true);
+
+    }
+}
