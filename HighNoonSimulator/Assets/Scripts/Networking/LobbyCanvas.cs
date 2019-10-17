bfbac4fdf52316e6ef8f579326197a1396c5007b
Chloe Culver <c.culver831@gmail.com>
2019-10-17 10:21:24 -0700

    Working on networking

--- HighNoonSimulator/Assets/Scripts/Networking/LobbyCanvas.cs
+++ HighNoonSimulator/Assets/Scripts/Networking/LobbyCanvas.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class LobbyCanvas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private RoomLayoutGroup _roomLayoutGroup;
+    private RoomLayoutGroup RoomLayoutGroup
     {
-        
+        get { return _roomLayoutGroup; }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void OnClickJoinRoom(string roomName)
     {
-        
+
     }
 }
