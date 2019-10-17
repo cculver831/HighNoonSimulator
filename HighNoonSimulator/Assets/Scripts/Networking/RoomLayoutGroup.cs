d0e046abbaf848f19be0cc08afec5bcb03722468
CCulver831 <c.culver831@gmail.com>
2019-10-16 22:54:31 -0700

    no message

---
+++ HighNoonSimulator/Assets/Scripts/Networking/RoomLayoutGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLayoutGroup : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _rommListingPrefab;
+    private GameObject RoomListingPrefab
+    {
+        get { return _rommListingPrefab; }
+    }
+}
---
+++ HighNoonSimulator/Assets/Scripts/Networking/RoomListing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RoomListing : MonoBehaviour
+{
+    [SerializeField]
+    private Text _roomNameText;
+    private Text RoomNameText
+    {
+        get { return _roomNameText; }
+    }
+    public bool Updated{ get; set; }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+}
